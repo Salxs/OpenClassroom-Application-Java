8cf2b225a065ca5577a58c41151bdcfdc4f97882
Salxs <shams.alexandre@reseau.eseo.fr>
2022-12-09 16:53:21 +0100

    Ajout des classe anonyme/ méthodes permettant de d'empecher l'utilisateur sans rentrer de nom et de détecter le click du bouton pour démarrer le jeu.

--- app/src/main/java/fr/shams/topquizz/MainActivity.java
+++ app/src/main/java/fr/shams/topquizz/MainActivity.java
@@ -2,7 +2,7 @@ package fr.shams.topquizz;
 
 import androidx.appcompat.app.AppCompatActivity;
 /*
-On importe les différentes classes nécessaires à la création et à l'utilisation de nos variables
+On importe les classes de nos différents widgets pour pouvoir les référencer
  */
 import android.os.Bundle;
 import android.widget.Button;
@@ -15,9 +15,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGreetingTextView =findViewById(R.id.main_textview_greeting);
+        mNameEditText = findViewById(R.id.main_edittext_name);
+        mPlayButton = findViewById(R.id.main_button_play);
     }
-    //Ajout des trois éléments graphique créés précedemment à l'aide de variable
+
+
+    //Ajout des trois éléments widgets créés précedemment à l'aide d'attribut de classe
     private TextView mGreetingTextView;
     private EditText mNameEditText;
     private Button mPlayButton;
+
+    //On crée les accesseurs de nos attributs de classe
+    public TextView getmGreetingTextView() {
+        return mGreetingTextView;
+    }
+
+    public EditText getmNameEditText() {
+        return mNameEditText;
+    }
+
+    public Button getmPlayButton() {
+        return mPlayButton;
+    }
+
+    //On crée les mutateurs de nos attributs de classe
+    public void setmGreetingTextView(TextView mGreetingTextView) {
+        this.mGreetingTextView = mGreetingTextView;
+    }
+
+    public void setmNameEditText(EditText mNameEditText) {
+        this.mNameEditText = mNameEditText;
+    }
+
+    public void setmPlayButton(Button mPlayButton) {
+        this.mPlayButton = mPlayButton;
+    }
+
 }
